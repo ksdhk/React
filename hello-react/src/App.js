78bc470f073d0f1903e9a26b419d87f3a4420d80
ksdhk <ksdhk@naver.com>
2021-02-28 02:36:41 +0900

    Update App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-const name = '리뒉트';
+const name = '리액트';
 
 return (
-  <div>
-  {name ==='리액트' ? (
-  <h1>리액트입니다.</h1>
-  ):(
-  <h2>리액트가 아닙니다.</h2>)
-  }
+  <div
+  style={{
+    backgroundColor : 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight : 'bold',
+    padding:16
+  }}
+  >
+   {name}
   </div>
 )
 }
