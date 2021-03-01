6764870ac18aa22f5374816647303cc1d3b97c7c
ksdhk <ksdhk@naver.com>
2021-03-01 23:36:55 +0900

    Update App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 
 function App() {
 const name = '리액트';
- return <div className="react">{name}</div>
+ return (
+ <>
+ {/*주석은 이렇게 작성*/}
+ <div className="react">{name}
+
+ </div>
+ //이런 주석이나
+ /*이런거는 다 보임 */
+</>
+ )
 }
 export default App;
