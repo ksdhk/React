ed764bf88edd33897583cecc707317e7da038231
ksdhk <ksdhk@naver.com>
2021-03-16 23:12:13 +0900

    Update Counter.js

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -4,15 +4,17 @@ class Counter extends Component{
     constructor(props){
         super(props);
         this.state = {
-            number:0
+            number:0,
+            fixedNumber:0
         }
     }
 
 render(){
-const { number } = this.state;
+const { number,fixedNumber} = this.state;
     return(
         <div>
         <h1> {number} </h1>
+        <h2> {fixedNumber}</h2>
         <button
         onClick = {()=>{
             this.setState ({ number:number+1 });
