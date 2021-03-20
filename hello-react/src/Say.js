8bf9f864811999becd1d3dcc5752c1d801171553
ksdhk <ksdhk@naver.com>
2021-03-20 22:45:31 +0900

    Update Say.js

--- hello-react/src/Say.js
+++ hello-react/src/Say.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 
 const Say = () => {
-    const {message, setMessage} = useState('');
+    const [ message , setMessage ] = useState('');
     const onClickEnter = () => setMessage('안녕하세요');
     const onClickLeave = () => setMessage('안녕히가세요!');
 
     return(
         <div>
-            <button onCllick={onClickEnter}>입장</button>
-            <button onCllick={onClickLeave}>퇴장</button>
-            <h1>message</h1>
+            <button onClick={onClickEnter}>입장</button>
+            <button onClick={onClickLeave}>퇴장</button>
+            <h1>{message}</h1>
         </div>
     )
 }
