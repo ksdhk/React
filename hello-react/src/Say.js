54d2ac4ddafa6c103ddaf79836b8a7fb17e6d08c
ksdhk <ksdhk@naver.com>
2021-03-21 23:05:45 +0900

    Update Say.js

--- hello-react/src/Say.js
+++ hello-react/src/Say.js
@@ -5,11 +5,24 @@ const Say = () => {
     const onClickEnter = () => setMessage('안녕하세요');
     const onClickLeave = () => setMessage('안녕히가세요!');
 
+    const [color, setColor] = useState('black');
+
+
     return(
         <div>
             <button onClick={onClickEnter}>입장</button>
             <button onClick={onClickLeave}>퇴장</button>
-            <h1>{message}</h1>
+            <h1 style={{color}}>{message}</h1>
+        <button style={{color:'red'}} onClilck={()=> setColor('red')}>
+        빨간색
+        </button>
+        <button style={{color:'green'}} onClilck={()=> setColor('green')}>
+        초록색
+        </button>
+        <button style={{color:'blue'}} onClilck={()=> setColor('blue')}>
+        파란색
+        </button>
+
         </div>
     )
 }
