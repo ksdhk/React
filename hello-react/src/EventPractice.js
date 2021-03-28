45a4aa645eea3b2df684f2ebf1eb970011f393d2
ksdhk <ksdhk@naver.com>
2021-03-29 00:20:44 +0900

    Update EventPractice.js

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -22,6 +22,15 @@ class EventPractice extends Component{
                     }
                 }
                 />
+                <button onClick={
+                    ()=>{
+                        alert(this.state.message);
+                        this.setState({
+                            message:''
+                        })
+                    }
+                }
+                >확인</button>
             </div>
         )
     }
