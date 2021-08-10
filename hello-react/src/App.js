1147701e9e547dfac10a5fb14eb6d698dd2fee5f
ksdhk <ksdhk@naver.com>
2021-08-11 01:33:26 +0900

    Update App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -6,13 +6,12 @@ class App extends Component{
     render(){
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                <header >
+                    <img src={logo} className="logo" alt="logo" />
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
                     <a
-                    classNAme = "App-link"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
